80ce172bafa4a1cd5182fe3a6b528f0d52056135
PhanVanTrung <vantrungphan2012@gmail.com>
2015-03-29 22:29:22 +0900

    add source code

---
+++ BitWatch/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BitWatch
+//
+//  Created by Phan Van Trung on 29/03/2015.
+//  Copyright (c) 2015 PVT Inc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+  
+}
+
